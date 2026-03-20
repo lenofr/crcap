7db68ba4e8f0981bf5c634a4a2fecac401e7332c
leno france <63382564+lenofr@users.noreply.github.com>
2026-03-20 18:01:12 -0300

    Delete debug-wpp2.php
    Delete

--- debug-wpp2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$log = __DIR__ . '/logs/whatsapp-debug.log';
-if (file_exists($log)) {
-    echo '<pre>' . htmlspecialchars(file_get_contents($log)) . '</pre>';
-} else {
-    echo 'Arquivo não encontrado: ' . $log;
-}
